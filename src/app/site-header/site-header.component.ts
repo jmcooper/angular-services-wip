4995c62bc31558fce217423a8ee81c68e188f134
Jim Cooper <jim@cooperplanet.com>
2024-03-14 13:35:45 -0600

    Add cart count to site header

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from '../user/user.model';
 import { UserService } from '../user/user.service';
+import { CartService } from '../cart/cart.service';
+import { Product } from '../catalog/product.model';
 
 @Component({
   selector: 'bot-site-header',
@@ -10,13 +12,15 @@ import { UserService } from '../user/user.service';
 export class SiteHeaderComponent implements OnInit {
   user: IUser | null = null;
   showSignOutMenu: boolean = false;
+  cart: Product[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private cartService: CartService) { }
 
   ngOnInit() {
     this.userService.getUser().subscribe({
       next: (user) => { this.user = user }
     })
+    this.cartService.cart.subscribe((cart) => this.cart = cart);
   }
 
   toggleSignOutMenu() {
