6f7cfda2112731704f5c854f51a4129f083bf540
Jim Cooper <jim@cooperplanet.com>
2024-03-20 11:38:38 -0600

    Add return type for getProducts()

--- src/app/catalog/products.service.ts
+++ src/app/catalog/products.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { productsArray } from './products-data';
+import { Product } from './product.model';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
-  getProducts() {
+  getProducts(): Product[] {
     return productsArray;
   }
 }
