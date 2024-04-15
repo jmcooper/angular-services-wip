922c7711fb640cb2b98ced37277f872a12c30611
Jim Cooper <jim@cooperplanet.com>
2024-04-15 14:54:18 -0600

    m3-c6: Calling APIs

--- src/app/catalog/products.service.ts
+++ src/app/catalog/products.service.ts
@@ -1,13 +1,14 @@
 import { Product } from '@shared/product.model';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
-  private products: Subject<Product[]> = new Subject();
+
+  constructor(private httpClient: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    fetch('/api/products')
-    return this.products;
+    return this.httpClient.get<Product[]>('/api/products');
   }
 }
