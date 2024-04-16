79304eec0a174537dba73a9ab03250ab402c6c72
Jim Cooper <jim@cooperplanet.com>
2024-04-16 09:56:07 -0600

    m5-c4: Creating multiple service instances

--- src/app/squad/squad.module.ts
+++ src/app/squad/squad.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { SquadRoutingModule } from './squad-routing.module';
 import { SquadCatalogComponent } from './squad-catalog/squad-catalog.component';
+import { CartService } from '@catalog/cart.service';
 
 @NgModule({
   declarations: [SquadCatalogComponent],
   imports: [SharedModule, SquadRoutingModule],
-  providers: [],
+  providers: [
+    {
+      provide: CartService,
+      useClass: CartService
+    },
+  ],
 })
 export class SquadModule { }
